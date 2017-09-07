063f5cf2c66d6eb8ad2431f35d7370ddc51765bf
Francisco Barra Sepúlveda <francisco.barra.sep@gmail.com>
2017-09-07 01:13:14 -0300

    Div Container

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './MainMenu.jsx';
+import App from './App.jsx';
 
+ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Main />, document.getElementById('mainContainer'));
